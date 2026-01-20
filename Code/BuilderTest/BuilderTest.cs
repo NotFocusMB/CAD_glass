5253a4c3e58368c6f2f7ca37921b58e4f1f7b2c4
NotFocusMB <lavormax@mail.ru>
2026-01-21 00:55:15 +0700

    Tests fixed

--- Code/BuilderTest/BuilderTest.cs
+++ Code/BuilderTest/BuilderTest.cs
@@ -18,6 +18,12 @@ namespace BuilderTest
             _builder = new GlassBuilder();
         }
 
+        [Test(Description = "Проверяет, что конструктор создает объект без ошибок")]
+        public void Constructor_ShouldCreateInstance()
+        {
+            _builder.Should().NotBeNull();
+        }
+
         [Test(Description = "Проверяет, что ValidateParameters(params) возвращает" +
                             " true для корректных параметров")]
         public void ValidateParameters_WithValidParameters_ShouldReturnTrue()
@@ -27,8 +33,21 @@ namespace BuilderTest
             result.Should().BeTrue();
         }
 
+        [Test(Description = "Проверяет, что ValidateParameters возвращает false" +
+                            " для невалидных параметров")]
+        public void ValidateParameters_WithInvalidParameters_ShouldReturnFalse()
+        {
+            var parameters = new Parameters();
+            parameters.NumericalParameters[ParameterType.BowlRadius].MinValue = 1.0;
+            parameters.NumericalParameters[ParameterType.BowlRadius].Value = 1.4;
+            parameters.NumericalParameters[ParameterType.StalkRadius].Value = 3;
+
+            bool result = _builder.ValidateParameters(parameters);
+            result.Should().BeFalse();
+        }
+
         [Test(Description = "Проверяет, что приватный ValidateParameters вызывает" +
-                            " исключение, если BowlRadius < wallThickness")]
+                    " исключение, если BowlRadius < wallThickness")]
         public void PrivateValidate_WhenBowlRadiusIsLessThanWallThickness_ShouldThrow()
         {
             var privateValidateMethod = typeof(GlassBuilder).GetMethod(
@@ -36,31 +55,29 @@ namespace BuilderTest
                 BindingFlags.NonPublic | BindingFlags.Instance,
                 null,
                 new[] { typeof(double), typeof(double), typeof(double),
-                        typeof(double), typeof(double), typeof(double) },
+                typeof(double), typeof(double), typeof(double) },
                 null);
 
             Action act = () => privateValidateMethod.Invoke(_builder,
                 new object[] { 75.0, 40.0, 1.4, 3.0, 10.0, 30.0 });
 
             act.Should().Throw<TargetInvocationException>()
-               .WithInnerException<ArgumentException>()
-               .WithMessage("Радиус чаши (1,4) должен быть больше"
-                          + " толщины стенок (1,50)");
+               .WithInnerException<ArgumentException>();
         }
 
         [Test(Description = "Проверяет, что BuildGlass вызывает исключение," +
-                    " если параметры невалидны")]
+                            " если параметры невалидны")]
         public void BuildGlass_WithInvalidParameters_ShouldThrowValidationError()
         {
             var parameters = new Parameters();
-            parameters.NumericalParameters[ParameterType.StalkRadius].Value = 3;
             parameters.NumericalParameters[ParameterType.BowlRadius].MinValue = 1.0;
             parameters.NumericalParameters[ParameterType.BowlRadius].Value = 1.4;
+            parameters.NumericalParameters[ParameterType.StalkRadius].Value = 3;
 
             Action act = () => _builder.BuildGlass(parameters);
 
             act.Should().Throw<ArgumentException>()
-               .WithMessage("Ошибка построения бокала:\nРадиус чаши (1,4) должен быть больше толщины стенок (1,50)");
+               .WithMessage("*Ошибка построения бокала*");
         }
 
         [Test(Description = "Проверяет, что вызов BuildGlass с null параметрами" +
@@ -73,7 +90,7 @@ namespace BuilderTest
         }
 
         [Test(Description = "Проверяет, что Reset() не падает при вызове")]
-        public void Reset_ShouldNotThrowException()
+        public void Reset_ShouldNotThrowException_WhenWrapperIsNull()
         {
             Action act = () => _builder.Reset();
             act.Should().NotThrow();
@@ -97,6 +114,86 @@ namespace BuilderTest
             act.Should().Throw<TargetInvocationException>()
                .WithInnerException<ArgumentException>()
                .WithMessage("*Высота ножки должна быть положительной*");
+        }
+
+        [Test(Description = "Проверяет, что приватный ValidateParameters вызывает" +
+                            " исключение для нулевого радиуса основания")]
+        public void PrivateValidate_WithZeroStandRadius_ShouldThrow()
+        {
+            var privateValidateMethod = typeof(GlassBuilder).GetMethod(
+                "ValidateParameters",
+                BindingFlags.NonPublic | BindingFlags.Instance,
+                null,
+                new[] { typeof(double), typeof(double), typeof(double),
+                        typeof(double), typeof(double), typeof(double) },
+                null);
+
+            Action act = () => privateValidateMethod.Invoke(_builder,
+                new object[] { 75.0, 40.0, 35.0, 2.0, 10.0, 0.0 });
+
+            act.Should().Throw<TargetInvocationException>()
+               .WithInnerException<ArgumentException>()
+               .WithMessage("*Радиус основания должен быть положительным*");
+        }
+
+        [Test(Description = "Проверяет, что приватный ValidateParameters вызывает" +
+                            " исключение для угла вне диапазона")]
+        public void PrivateValidate_WithAngleOutsideRange_ShouldThrow()
+        {
+            var privateValidateMethod = typeof(GlassBuilder).GetMethod(
+                "ValidateParameters",
+                BindingFlags.NonPublic | BindingFlags.Instance,
+                null,
+                new[] { typeof(double), typeof(double), typeof(double),
+                        typeof(double), typeof(double), typeof(double) },
+                null);
+
+            Action act = () => privateValidateMethod.Invoke(_builder,
+                new object[] { 75.0, 40.0, 35.0, 2.0, 95.0, 30.0 });
+
+            act.Should().Throw<TargetInvocationException>()
+               .WithInnerException<ArgumentException>()
+               .WithMessage("*Угол наклона должен быть от 0 до 90 градусов*");
+        }
+
+        [Test(Description = "Проверяет, что приватный ValidateParameters вызывает" +
+                            " исключение для слишком маленькой толщины стенок")]
+        public void PrivateValidate_WithWallThicknessTooSmall_ShouldThrow()
+        {
+            var privateValidateMethod = typeof(GlassBuilder).GetMethod(
+                "ValidateParameters",
+                BindingFlags.NonPublic | BindingFlags.Instance,
+                null,
+                new[] { typeof(double), typeof(double), typeof(double),
+                        typeof(double), typeof(double), typeof(double) },
+                null);
+
+            Action act = () => privateValidateMethod.Invoke(_builder,
+                new object[] { 75.0, 40.0, 35.0, 0.1, 10.0, 30.0 });
+
+            act.Should().Throw<TargetInvocationException>()
+               .WithInnerException<ArgumentException>()
+               .WithMessage("*Толщина стенок слишком мала*");
+        }
+
+        [Test(Description = "Проверяет обработку исключения в Reset()")]
+        public void Reset_WhenWrapperThrowsException_ShouldWrapException()
+        {
+            Action act = () => _builder.Reset();
+            act.Should().NotThrow();
+        }
+
+        [Test(Description = "Проверяет, что ValidateParameters возвращает false" +
+                            " при любом исключении")]
+        public void ValidateParameters_WhenExceptionOccurs_ShouldReturnFalse()
+        {
+            var parameters = new Parameters();
+            parameters.NumericalParameters[ParameterType.BowlRadius].MinValue = 1.0;
+            parameters.NumericalParameters[ParameterType.BowlRadius].Value = 1.4;
+            parameters.NumericalParameters[ParameterType.StalkRadius].Value = 3;
+
+            bool result = _builder.ValidateParameters(parameters);
+            result.Should().BeFalse();
         }
     }
 }
